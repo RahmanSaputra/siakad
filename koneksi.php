80e989a3c5f98bf66442579a8e77a23ef2763ada
RahmanSaputra <oksigenoffice@gmail.com>
2013-07-18 00:45:15 +0700

    18 Jul

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = @mysql_connect('localhost','h113330_root','awerasdfgh');
-if (!$conn) {
-	die('Could not connect: ' . mysql_error());
-}
-mysql_select_db('h113330_siakad', $conn);
-
-?>
